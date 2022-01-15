9eac32f56e614f9c25c6e79fb513753687b85a5f
NgXThaiT2110E <92872465+NgXThaiT2110E@users.noreply.github.com>
2022-01-15 20:16:19 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import {StudentComponent} from "./student/student.component";
 import {ClassroomComponent} from "./classroom/classroom.component";
 import {Assignment1Componen} from "./assignment1/assignment1.componen";
 import {PracticingComponent} from "./practicing test/practicing.component";
+import {Assignment3Component} from "./assignment3/assignment3component";
+import {Assignment4component} from "./assignment4/assignment4component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent,ClassroomComponent,StudentComponent,Assignment1Componen,PracticingComponent
+    DemoComponent,ClassroomComponent,StudentComponent,Assignment1Componen,PracticingComponent,
+    Assignment3Component,Assignment4component
   ],
   imports: [
     BrowserModule
--- src/app/practicing test/practicing.component.ts
+++ src/app/practicing test/practicing.component.ts
@@ -6,5 +6,8 @@ import {Component} from "@angular/core";
   styleUrls:["./practicing.component.css"]
 })
 export class PracticingComponent{
-
+  @ViewChild("f") createForm!: NgForm;
+  onSubmit(){
+    console.log(this.createForm.value);
+  }
 }
