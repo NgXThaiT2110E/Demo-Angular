9d4bcc69c54376c9ec15633cea68037e9cf181bb
NgXThaiT2110E <92872465+NgXThaiT2110E@users.noreply.github.com>
2022-01-15 19:27:56 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import {DemoComponent} from "./demo/demo.component";
 import {StudentComponent} from "./student/student.component";
 import {ClassroomComponent} from "./classroom/classroom.component";
 import {Assignment1Componen} from "./assignment1/assignment1.componen";
+import {PracticingComponent} from "./practicing test/practicing.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent,ClassroomComponent,StudentComponent,Assignment1Componen
+    DemoComponent,ClassroomComponent,StudentComponent,Assignment1Componen,PracticingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/practicing test/practicing.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:"practicing",
+  templateUrl:"./practicing.component.html",
+  styleUrls:["./practicing.component.css"]
+})
+export class PracticingComponent{
+
+}
